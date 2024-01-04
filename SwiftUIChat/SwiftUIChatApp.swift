84e43e35e544f0abae52a4ab5215833006a67afa
Siam300 <89306483+Siam300@users.noreply.github.com>
2024-01-04 10:51:16 +0600

    Initial Commit

---
+++ SwiftUIChat/SwiftUIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChatApp.swift
+//  SwiftUIChat
+//
+//  Created by Auto on 4/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
