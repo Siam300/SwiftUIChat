6c5aa8851bf39b9b367b35753695c6d0ac2fb89f
Siam300 <89306483+Siam300@users.noreply.github.com>
2024-01-04 10:53:37 +0600

    SwuftUI Chat project created

---
+++ SwiftUIChat/View/LogInView.swift
@@ -0,0 +1,18 @@
+//
+//  LogInView.swift
+//  SwiftUIChat
+//
+//  Created by Auto on 4/1/24.
+//
+
+import SwiftUI
+
+struct LogInView: View {
+    var body: some View {
+        Text("LogIn View")
+    }
+}
+
+#Preview {
+    LogInView()
+}
---
+++ SwiftUIChat/View/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  SwiftUIChat
+//
+//  Created by Auto on 4/1/24.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text("Registration View")
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
